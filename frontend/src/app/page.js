d150ebd364368ed6ec77cc99cd775ebe342ede2f
Aditya Thute <aadityathute@gmail.com>
2025-06-14 11:26:21 +0530

    Step-63 Added Designs - Frontend

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <h1>Welcome to Shivanya Multiservices</h1>
-      <p>Track your income, expenses, and balance.</p>
+      <p>Track your income, expenses, services and balance.</p>
     </>
   );
 }
